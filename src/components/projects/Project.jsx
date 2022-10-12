cdf49cf8ff1d510fda238207174620ab2b5b8bf3
alcides_t <alcides.dml@icloud.com>
2022-10-12 14:33:18 +0900

    Added more content to the project page and styled the images

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,9 +1,19 @@
+import { useRef } from 'react'
 import React from 'react'
 import './Project.css'
 import picture1 from '../../assets/notes-taking-ui.jpg'
 import picture2 from '../../assets/name-tag-generator.png'
+import picture3 from '../../assets/guess-the-word.jpg'
+import picture4 from '../../assets/unplugged-website.jpg'
+import picture5 from '../../assets/rogue-pickings.jpg'
 
 const Project = () => {
+    
+    const myRef = useRef();
+    
+
+
+
   return (
     <section id="project">
         <h5>My favorite projects</h5>
@@ -36,16 +46,38 @@ const Project = () => {
             <article className="fade-left">
             <div className="image-holder">
                     <image>
-                        <img></img>
+                        <img src={picture3} alt="Notes-Taking_Ui"/>
                     </image>
-                </div> 
+                </div>
+                <div className='project-container'>
+                <span className='title'>Guess The Word Game</span><br></br>
+                <span className='project-item'>SkillCrush Project</span><br></br>
+                <span className='project-text'>Players guess the word by entering one letter at a time. If the player guesses all the letters correctly before they use up their allotted guesses, they win!</span>
+                </div>
             </article>
             <article className="fade-right">
             <div className="image-holder">
                     <image>
-                        <img></img>
+                        <img src={picture4} alt="Notes-Taking_Ui"/>
                     </image>
-                </div> 
+                </div>
+                <div className='project-container'>
+                <span className='title'>Unplugged Website</span><br></br>
+                <span className='project-item'>SkillCrush Project</span><br></br>
+                <span className='project-text'>A multi-page responsive website coded with Flexbox. This versatile design has several sections, including a Cards design pattern, that can be repurposed for different sites.</span>
+                </div>
+            </article>
+            <article className="fade-left">
+            <div className="image-holder">
+                    <image>
+                        <img src={picture5} alt="Notes-Taking_Ui"/>
+                    </image>
+                </div>
+                <div className='project-container'>
+                <span className='title'>Rogue Pickings Website</span><br></br>
+                <span className='project-item'>SkillCrush Project</span><br></br>
+                <span className='project-text'>A simple home page perfect for a pop-up restaurant or food truck. Originally a static website, I converted it into a responsive design using Flexbox.</span>
+                </div>
             </article>
         </div>
     </section>
